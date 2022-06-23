63f0363898fd3854ff2cc4c91b92dfaa87b210c7
DevMinGeonPark <devmin7985@gmail.com>
2022-06-23 17:14:53 +0900

    API 문서 title change

--- swagger.js
+++ swagger.js
@@ -1,47 +1,22 @@
 const swaggerUi = require('swagger-ui-express');
 const swaggereJsdoc = require('swagger-jsdoc');
 const swaggerAutogen = require('swagger-autogen')({ openapi: '3.0.0' });
-const options = {
-    swaggerDefinition: {
-        info: {
-            title: 'EcoFit API',
-            version: '1.0.1',
-            description: 'EcoFit API',
-        },
-        servers:[
-            {
-                url:'http://localhost:3000'
-            },
-        ],
-        schemes: ['http'],
-        securityDefinitions: {
-            bearerAuth: {
-              type: 'http',
-              scheme: 'bearer',
-              in: 'header',
-              bearerFormat: 'JWT',
-            },
-          },
-    },
-    apis: ['./routes/index.js', './swagger/*']
-};
 
-// const options = {
-//     info: {
-//       title: 'My API',
-//       description: 'Description',
-//     },
-//     host: 'localhost:3000',
-//     schemes: ['http'],
-//   };
+
+const options = {
+    info: {
+        title: 'EcoFit Rest API',
+        version: '1.0.0',
+        description: 'SW 중심대학 통합 해커톤 EcoFit 프로젝트 Rest API Dcoument',
+    },
+    servers:[
+        {
+            url:'http://localhost:3000'
+        },
+    ],
+    schemes: ['http'],
+  };
 
 const outputFile = './swagger-output.json';
 const endpointsFiles = ['./src/routes/index.js'];
 swaggerAutogen(outputFile, endpointsFiles, options);
-
-const specs = swaggereJsdoc(options);
-
-module.exports = {
-    swaggerUi,
-    specs
-};
