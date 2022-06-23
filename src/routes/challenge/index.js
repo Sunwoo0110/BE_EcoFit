362cbb908024884e4df567515ec316402ab6d82f
sunwookim <sunkim09@g.skku.edu>
2022-06-24 01:22:50 +0900

    FEAT fix error

--- src/routes/challenge/index.js
+++ src/routes/challenge/index.js
@@ -4,9 +4,9 @@ const ctrl = require("./challenge.ctrl");
 
 // Get
 router.get("/", ctrl.get_root);
-router.get("/add/:title", ctrl.get_click_title);
+router.get("/click/:title", ctrl.get_click_title);
 
 // Put
-router.put("/click/:title", ctrl.put_join);
+router.put("/add/:title", ctrl.put_join);
 
 module.exports = router;
