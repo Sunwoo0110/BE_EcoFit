0c24e8cb35904817ad0be300e7f94657fbe3b208
DevMinGeonPark <devmin7985@gmail.com>
2022-06-24 01:40:32 +0900

    bug fit

--- src/routes/challenge/index.js
+++ src/routes/challenge/index.js
@@ -12,10 +12,6 @@ router.get("/", ctrl.get_root);
 router.get("/click/:title", ctrl.get_click_title);
 
 // Put
-<<<<<<< HEAD
-router.put("/add/:title", ctrl.put_join);
-=======
-router.put("/click/:title", upload.single('img'), ctrl.put_join);
->>>>>>> a6d5b88e2a813bef6e737020a0a09cb5c27b56e1
+router.put("/add/:title", upload.single('img'), ctrl.put_join);
 
 module.exports = router;
